e3a172890b4a33f96616abb8bb26ddaaaea26ebc
shababrc <chowdhuryshababrob@gmail.com>
2023-03-22 13:54:23 -0700

    rought user inputs template and js file for variable saving

---
+++ userinputs.js
@@ -0,0 +1,10 @@
+var userHealth = [];
+
+var height = document.getElementById("height").value;
+
+var weight = document.getElementById("weight").value;
+
+var gender = document.getElementById("gender").value;
+
+var age = document.getElementById("age").value;
+
