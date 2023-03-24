98b65138a2612f352cb088e9ac270a07555464bf
Harold Gonzales <haroldgonzales@yahoo.com>
2023-03-24 11:10:50 -0700

    Integrate the Search button of Nutritionix and clean up the html and java script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,4 @@
-// // const apiKey = '6b6d64cee3b28347ac2bcf9249459402';
-// // const appId = '494da15a';
 
-// // fetch(`https://api.nutritionix.com/v1_1/search?q=banana&appId=${appId}&appKey=${apiKey}`)
-// //   .then(response => response.json())
-// //   .then(data => console.log(data))
-// //   .catch(error => console.error(error));
-
-
-
-//Shabab's Edamame api call, edited to utilize Food Database
-//changed app keys and id to match Food Database API
-var appedaKey = '96c9812e34d70f8817c3c0855d4ebb4a';
-var appeda_Id = 'a230f40d';
-var ingredients = document.getElementById("ingredient");
-var submitBtn = document.getElementById("submitBmrBtn");
-
-submitBtn.addEventListener('click', getResults);
-function getResults(){
-
-//sample fetch request
-fetch('https://api.edamam.com/api/food-database/v2/parser?app_id=' + appeda_Id + '&app_key=' + appedaKey + '&ingr=' + ingredients, {
-  method: 'GET', //GET is the default.
-})
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    console.log(data.calories); //console logging accessing calories from data response
-  }); 
-}
 //created by harold
 var bmr = 0;
 var bmi = 0;
@@ -54,59 +23,17 @@ function bmrbmicalc() {
 return ;
 }
 
-// //This function will calculate the BMI by Harold
-// function calculateBMI() {
-
-//   //this will get the value of the weight and height 
-//   var weight = (document.getElementById("weight").value);
-//   var height = (document.getElementById("height").value);
-  
-//   //this will calculate the bmi and using "tofixed" to get only 1 decimal place
-//   var bmi = weight / (height * height);
-//   bmi = bmi.toFixed(1);
-  
-//   //this will send the output to the screen
-//   var result = document.getElementById("result");
-//   result.innerHTML = "Your BMI is " + bmi + "";
-// }
-
-
-//Edamame api call, edited to utilize Food Database
-// //changed app keys and id to match Food Database API
-// var appedaKey = '96c9812e34d70f8817c3c0855d4ebb4a';
-// var appeda_Id = 'a230f40d';
-// var ingredients = document.getElementById("ingredient");
-// var submitBtn = document.getElementById("submitBmrBtn");
-
-// submitBtn.addEventListener('click', getResults);
-// function getResults(){
-
-// //sample fetch request
-// fetch('https://api.edamam.com/api/food-database/v2/parser?app_id=' + appeda_Id + '&app_key=' + appedaKey + '&ingr=' + ingredients, {
-//   method: 'GET', //GET is the default.
-// })
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data);
-//     console.log(data.calories); //console logging accessing calories from data response
-//     console.log(data.totalNutrientsKCal);
-//   }); 
-// }
-
-
 //possible duplicate (extra console log)
 var appedaKey = '96c9812e34d70f8817c3c0855d4ebb4a';
 var appeda_Id = 'a230f40d';
 var ingredients = document.getElementById("ingredient");
-var submitBtn = document.getElementById("submitBmrBtn");
+var searchBtn = document.getElementById("searchBtn");
 
-submitBtn.addEventListener('click', getResults);
+searchBtn.addEventListener('click', getResults);
 
 function getResults() {
   //sample fetch request
-  fetch('https://api.edamam.com/api/food-database/v2/parser?app_id=' + appeda_Id + '&app_key=' + appedaKey + '&ingr=' + ingredients.value, {
+  fetch('https://api.edamam.com/api/food-database/v2/parser?app_id=' + appeda_Id + '&app_key=' + appedaKey + '&ingr=' + searchInput.value, {
     method: 'GET', //GET is the default.
   })
     .then(function (response) {
@@ -118,3 +45,71 @@ function getResults() {
       console.log(data.totalNutrientsKCal);
     }); 
 }
+
+//Created by mNova
+var appedaKey = '96c9812e34d70f8817c3c0855d4ebb4a';
+var appeda_Id = 'a230f40d';
+var searchInput = document.getElementById("searchInput");
+var searchBtn = document.getElementById("searchBtn");
+
+searchBtn.addEventListener('click', getEdamamResults);
+
+
+let energyCals = 0;
+let proteinCals = 0;
+let carbCals = 0;
+let fatCals = 0;
+function getEdamamResults() {
+
+fetch('https://api.edamam.com/api/food-database/v2/parser?app_id=' + appeda_Id + '&app_key=' + appedaKey + '&ingr=' + searchInput.value, {
+  method: 'GET', //GET is the default.
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    console.log(data.parsed[0].food.nutrients);
+
+    energyCals = data.parsed[0].food.nutrients.ENERC_KCAL;
+    proteinCals = data.parsed[0].food.nutrients.PROCNT;
+    carbCals = data.parsed[0].food.nutrients.FAT;
+    fatCals = data.parsed[0].food.nutrients.PROCNT;
+  });
+}
+  //created by aarellano
+
+var searchInput = document.getElementById('searchInput')
+var searchBtn = document.getElementById('searchBtn')
+var searchResult = document.getElementById('searchResult')
+searchBtn.addEventListener('click', getNutrixResults)
+function getNutrixResults() {
+  var apiKey = '6b6d64cee3b28347ac2bcf9249459402';
+  var appId = '494da15a';
+  var query = searchInput.value
+  fetch(`https://trackapi.nutritionix.com/v2/search/instant?query=${query}`, {
+    method: 'GET',
+    headers: {
+      'x-app-id': appId,
+      'x-app-key': apiKey,
+      'x-remote-user-id': '0',
+    },
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    // Do something with the returned data
+    foodName = data.common[0].food_name
+    image = data.common[0].photo.thumb
+    displayNutrix(image, foodName)
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+}
+function displayNutrix(image, foodName) {
+  let html = `<p>Food Name: ${foodName}</p>
+              <img src="${image}">`;
+              searchResult.innerHTML = html;
+}
+
